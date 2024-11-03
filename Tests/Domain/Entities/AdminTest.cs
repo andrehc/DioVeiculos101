82b5bf2a0420b2b8e088cd0cec101a07c74b9491
andre costa <andre.hcosta@hotmail.com>
2024-11-03 10:37:34 -0300

    refatoracao solution; teste de unidade

---
+++ Tests/Domain/Entities/AdminTest.cs
@@ -0,0 +1,26 @@
+﻿
+using DioVeiculos101.Domain.Entity;
+
+namespace Tests.Domain.Entities
+{
+    public class AdminTest
+    {
+        [TestMethod]
+        public void TestGetSetProperties()
+        {
+            var admin = new Admin();
+
+            admin.Id = 1;
+            admin.Email = "lorem@ipsum.com";
+            admin.Name = "lorem Ipsum";
+            admin.Profile = "admin";
+            admin.Password = "102030";
+
+            Assert.AreEqual(1, admin.Id);
+            Assert.AreEqual("lorem@ipsum", admin.Email);
+            Assert.AreEqual("lorem ipsum", admin.Name);
+            Assert.AreEqual("admin", admin.Profile);
+            Assert.AreEqual("102030", admin.Password);
+        }
+    }
+}
---
+++ Tests/Domain/Entities/VehicleTest.cs
@@ -0,0 +1,23 @@
+﻿using DioVeiculos101.Domain.Entity;
+
+namespace Tests.Domain.Entities
+{
+    public class VehicleTest
+    {
+        public void TestGetSetVehicleProperties()
+        {
+            var v = new Vehicle();
+
+            v.Id = 1;
+            v.Brand = "Lorem";
+            v.Name = "Ipsum";
+            v.Year = 12;
+
+            Assert.AreEqual(1, v.Id);
+            Assert.AreEqual("Lorem", v.Brand);
+            Assert.AreEqual("Ipsum", v.Name);
+            Assert.AreEqual(12, v.Year);
+
+        }
+    }
+}
